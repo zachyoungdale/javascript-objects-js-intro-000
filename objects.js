2a1ab0e9ab129dc04ed1f6f5e4f7a8d5754a658d
Zachary Youngdale <zyoungdale@gmail.com>
2021-06-18 01:20:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,6 @@ var playlist = new Object({artisName: "songTitle"})
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Slowdive'] = 'Alison'
   playlist['My Bloody Valentine'] = 'Sometimes'
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
   return playlist
 }
