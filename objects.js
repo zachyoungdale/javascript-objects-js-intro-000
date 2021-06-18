5abd8d792b757d4fe1e23c3ba1ab620c7bf12586
Zachary Youngdale <zyoungdale@gmail.com>
2021-06-18 01:18:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
 var playlist = new Object({artisName: "songTitle"})
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist['Slowdive'] = 'Alison'
+  playlist['My Bloody Valentine'] = 'Sometimes'
+  return playlist
+}
